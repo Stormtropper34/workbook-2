cc1a6ff3563f8aa4571a1639bf81b4e1376fde2f
Josselyn Mejia <josselynmejia572@gmail.com>
2025-04-22 09:55:16 -0400

    TestStatistics

--- TestStatistics/src/main/java/com/pluralsight/TestStatistics.java
+++ TestStatistics/src/main/java/com/pluralsight/TestStatistics.java
@@ -1,20 +1,29 @@
 package com.pluralsight;
 
-import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
 
 public class TestStatistics {
     public static void main(String[] args) {
 
-        int[] testScores = {10, 20, 35, 75, 88, 90, 90, 78, 100, 77,22};
+        int[] testScores = {10, 20, 35, 75, 88, 90, 90, 78, 100, 77, 22};
+        Arrays.sort(testScores);
 
+        double total = 0;
+        for (int testScore : testScores) {
+            total += testScore;
 
-        int highScore = highTestScore(testScores);
-        int lowScore = lowTestScore(testScores);
-        double averageScore; = averageTestScore(testScores);
+        }
+        int numOfScores = testScores.length;
+        double average = total/ testScores.length;
+        System.out.printf("Average test score: %.2f\n", average);
 
+        int lowScore = testScores[0];
+        int highScore = testScores[testScores.length-1];
 
+        System.out.println("Low score: " + lowScore);
+        System.out.println("High Score: " + highScore);
 
-public static int highTestScore(int [] score) {
 
 
 
